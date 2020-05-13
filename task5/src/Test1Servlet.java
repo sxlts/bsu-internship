153b6e02e5f9e678e1d008c1160aade6cc7a60da
soleni <k1a3w3a7i1@gmail.com>
2020-05-13 03:17:22 +0300

    add servlets

---
+++ task5/src/Test1Servlet.java
@@ -0,0 +1,16 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Test1Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = getServletContext();
+        RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/status");
+        requestDispatcher.forward(req, resp);
+    }
+}
